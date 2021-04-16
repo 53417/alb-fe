fdc064cde5b14000a69a67e4f786328d3b04df6d
01 <a01@s01.local>
2021-04-16 15:33:40 +1000

    link heroku

--- src/services/sql-test.service.js
+++ src/services/sql-test.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:53416/api/sqlTest/";
+const API_URL = "https://alb-be.herokuapp.com/api/sqlTest/";
 
 class sqlTestService {
   executeQuery(query) {
